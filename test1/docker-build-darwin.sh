6ceac584e5c1a13ea5067afe3aee338644244561
zppro <zpprozppro@gmail.com>
2019-05-05 13:39:22 +0800

    更新脚本

--- test1/docker-build-darwin.sh
+++ test1/docker-build-darwin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 outfile=$(basename `pwd`)
-docker run -v "$GOPATH":/go --rm -v "$PWD":/go/src/test1 -w /go/src/test1 -e GOOS="darwin" -e GOARCH="amd64" golang:1.11.6 go build -v -o "$outfile" 
+docker run -v "$GOPATH":/go --rm -v "$PWD":"/go/src/$outfile" -w "/go/src/$outfile" -e GOOS="darwin" -e GOARCH="amd64" golang:1.11.6 go build -v -o "$outfile"
 fhash=`shasum -a 256 ./$outfile`
 mv "$outfile" "$outfile-darwin-${fhash:0:8}"
 unset fhash
--- test1/docker-build-linux.sh
+++ test1/docker-build-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 outfile=$(basename `pwd`)
-docker run -v "$GOPATH":/go --rm -v "$PWD":/go/src/test1 -w /go/src/test1 -e GOOS="linux" -e GOARCH="amd64" golang:1.11.6 go build -v -o "$outfile" 
+docker run -v "$GOPATH":/go --rm -v "$PWD":"/go/src/$outfile" -w "/go/src/$outfile" -e GOOS="linux" -e GOARCH="amd64" golang:1.11.6 go build -v -o "$outfile"
 fhash=`shasum -a 256 ./$outfile`
 mv "$outfile" "$outfile-linux-${fhash:0:8}"
 unset fhash
